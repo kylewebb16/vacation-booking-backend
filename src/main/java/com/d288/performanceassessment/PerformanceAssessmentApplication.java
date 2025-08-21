7b2dd1c33ddaa1ca3ccc6ec944401f14c85bf2c4
Kyle Webb <kyle.jo.webb@gmail.com>
2025-08-21 11:59:46 -0400

    Part A

---
+++ src/main/java/com/d288/performanceassessment/PerformanceAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.d288.performanceassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d288/performanceassessment/PerformanceAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.performanceassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
